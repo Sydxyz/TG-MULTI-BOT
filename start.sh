46e41cbeb84254340cf9b12f8e3a7fd015f87f56
Sydxyz <109050076+Sydxyz@users.noreply.github.com>
2023-06-28 07:19:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
-apt update && apt upgrade -y
-apt install ffmpeg -y
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Sydxyz/TG-MULTI-BOT.git /DQ-The-File-Donor
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+fi
+cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
-echo "Starting the Bot...."
+echo "Starting DQ-The-File-Donor...."
 python3 loader.py
